7a16171186a985d682f851468471975a838980aa
임창준 <icj4153@hanyang.ac.kr>
2023-09-18 17:48:30 +0900

    [K3-23] [K3-40] feat : art and study Crawling 개발 완료

--- crawling/src/main/java/dev/ioexception/crawling/page/site/ArtandStudyCrawling.java
+++ crawling/src/main/java/dev/ioexception/crawling/page/site/ArtandStudyCrawling.java
@@ -1,6 +1,7 @@
 package dev.ioexception.crawling.page.site;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -11,51 +12,85 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.ioexception.crawling.entity.Lecture;
 import dev.ioexception.crawling.entity.LectureTag;
 import dev.ioexception.crawling.entity.Tag;
+import dev.ioexception.crawling.page.UploadImage;
 import dev.ioexception.crawling.repository.LectureRepository;
+import dev.ioexception.crawling.repository.LectureTagRepository;
+import dev.ioexception.crawling.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class ArtandStudyCrawling {
 
 	private final LectureRepository lectureRepository;
+	private final LectureTagRepository lectureTagRepository;
+	private final TagRepository tagRepository;
+	private final UploadImage uploadImage;
 
 	public List<Lecture> getSaleLecture() throws IOException {
 		List<Lecture> lectureList = new ArrayList<>();
 
+		String [][] sub_category = { {},
+			{"","철학입문", "서양고대철학", "근대현대철학", "동양철학", "정신분석/심리학", "윤리학","정치철학"},
+			{"","시", "소설", "문학일반", "문예창작"},
+			{"","미술","미학","문화","음악","영화","사진","건축"},
+			{"","고전","역사","과학","종교/신화","사회/경제","교양인문학"}
+		};
 
+
+		// 강의 카테고리 생성
 		for(int i = 1; i<=4;i++){
 			Document document = Jsoup.connect("https://www.artnstudy.com/n_lecture/LecList.asp?LessonPart=" + i + "00").get();
+			// 각 카테고리 별 세부 카테고리
+			int categorylen = document.select("#lnb > ul > a").size();
+			System.out.println("categorylen = " + categorylen);
 
-			// 태그르 넣어준다.
-			Tag tag = Tag.builder()
-				.name(document.select("#topmenu1 > a").text())
-				.build();
+			for (int j = 1; j <= categorylen; j++){
+				document = Jsoup.connect("https://www.artnstudy.com/n_lecture/LecList.asp?LessonPart=" + i + "0" + j).get();
 
-			Elements contents = document.select(
-				"#content > div.wrap > div > div > ul > li:nth-child(4) > ul > a");
-			for (Element content : contents) {
-				Lecture lecture = Lecture.builder()
-					.lecture_Id(getLectureId(content))
-					.title(getTitle(content))
-					.instructor(getInstructor(content))
-					.company_name("ArtandStudy")
-					.ordinary_price(getPrice(content))
-					.sale_price(getSalePrice(content))
-					.sale_percent(getSalePercent(content))
-					.site_link(getUrl(content))
-					.image_link(getImage(content))
-					.build();
-				lectureList.add(lecture);
-				lectureRepository.save(lecture);
+				System.out.println(sub_category[i][j]);
+
+				// 태그를 넣어준다.
+				// 먼저 태그가 이미 존재하는지 확인하고, 존재한다면 해당 태그를 가져옴
+				String sub_cat_name = sub_category[i][j];
+				Tag tag = tagRepository.findByName(sub_category[i][j])
+					.orElseGet(() -> tagRepository.save(Tag.builder().name(sub_cat_name).build()));
+
+				Elements contents = document.select(
+					"#content > div.wrap > div > div > ul > li:nth-child(4) > ul > a");
+				for (Element content : contents) {
+					Lecture lecture = Lecture.builder()
+						.lecture_Id(getLectureId(content))
+						.title(getTitle(content))
+						.instructor(getInstructor(content))
+						.company_name("ArtandStudy")
+						.ordinary_price(getPrice(content))
+						.sale_price(getSalePrice(content))
+						.sale_percent(getSalePercent(content))
+						.site_link(getUrl(content))
+						.image_link(getImage(content))
+						.date(LocalDate.now())
+						.build();
+					// lectureList.add(lecture);
+
+					lectureRepository.save(lecture);
+
+					// 강의 태그 중간테이블을 저장한다.
+					LectureTag lectureTag = new LectureTag();
+					lectureTag.setTag(tag);
+					lectureTag.setLecture(lecture);
+					lectureTagRepository.save(lectureTag);
+				}
 			}
 		}
 
-		return lectureList                                       ;
+		return lectureList;
 	}
 
 	private String getLectureId(Element content){
@@ -75,17 +110,22 @@ public class ArtandStudyCrawling {
 
 	}
 
-	private String getSalePrice(Element content) {
-		Elements elements = content.select("li > ul > li:last-child > ul > li:nth-child(3) > span");
+	private int getSalePrice(Element content) {
+		Elements elements = content.select("li > ul > li:nth-child(2) > ul > li.viewsale > span");
+
 		if (elements.isEmpty()) {
 			return getPrice(content);
 		} else if (elements.text().contains("강좌추가")) {
 			return getPrice(content);
 		} else{
-			return elements.text();
+			String salePrice = elements.text();
+			salePrice = salePrice.replaceAll("[^0-9]", "");
+			int salePriceInt = Integer.parseInt(salePrice);
+
+			return salePriceInt;
 		}
 	}
-	private String getImage(Element content) {
+	private String getImage(Element content) throws IOException {
 		// 이미지를 가져온다.
 		Element imgElement =  content.selectFirst("li > ul > li:nth-child(1) > div");
 		// Style 속성값을 가져온다.
@@ -98,20 +138,25 @@ public class ArtandStudyCrawling {
 		if(imageUrl.equals("/image/photo_b/sub/njPark003_2.jpg")){
 			imageUrl = "/image/photo_b/sub/njPark006_2.jpg";
 		}
+		imageUrl = "https://www.artnstudy.com" + imageUrl;
 
-		return "https://www.artnstudy.com" + imageUrl;
+		return uploadImage.uploadFromUrlToS3(imageUrl, "artandstudy", getLectureId(content));
 	}
 	private String getUrl(Element content){
 		// a태그의 href 속성을 가져온다.
 		String classUrl = content.attr("href");
 		return "https://www.artnstudy.com" + classUrl;
 	}
-	private String getPrice(Element content) {
+	private int getPrice(Element content) {
 		String price = content.select("li > ul > li:last-child > ul > li:nth-child(2) > span").text();
-		if(price.equals("0원")){
-			price = "무료";
-		}
-		return price;
+		int priceInt = 0;
+		// if(price.equals("0원")){
+		// 	price = "무료";
+		// }
+		String numericPart = price.replaceAll("[^0-9]", "");
+		priceInt = Integer.parseInt(numericPart);
+
+		return priceInt;
 	}
 	private String getTitle(Element content){
 		return content.select("li > ul > li:last-child > ul > li:nth-child(1) > ul > li").text();
@@ -121,23 +166,17 @@ public class ArtandStudyCrawling {
 		instructor = instructor.substring(0,3);
 		return instructor;
 	}
-	private String getSalePercent(Element content) {
-		String price = getPrice(content);
-		String salePrice = getSalePrice(content);
+	private String  getSalePercent(Element content) {
+		int price = getPrice(content);
+		int salePrice = getSalePrice(content);
 
-		if (salePrice.equals(price)) {
+		if (salePrice == price) {
 			return "-1";
 		}else{
-			double priceValue = Double.parseDouble(price.replaceAll("[^\\d.]", ""));
-			double salePriceValue = Double.parseDouble(salePrice.replaceAll("[^\\d.]", ""));
+			double discountPercentage = ((double) (price - salePrice) / price) * 100.0;
+			int salePercent = (int) Math.round(discountPercentage);
 
-			double discountPercentage = ((priceValue - salePriceValue) / priceValue) * 100.0;
-			double roundedDiscountPercentage = Math.round(discountPercentage); // 반올림
-
-			String discountPercentageStr = String.format("%.0f", roundedDiscountPercentage) + "%";
-
-
-			return discountPercentageStr;
+			return salePercent + "%";
 		}
 	}
 }
