55e4e3e4dcb274da9482d72aedc88e3154d5f2cf
BHC-Chicken <tij05078@gmail.com>
2023-09-19 14:52:29 +0900

    fix: image 스트림 변경 시 예외처리 추가

--- crawling/src/main/java/dev/ioexception/crawling/page/UploadImage.java
+++ crawling/src/main/java/dev/ioexception/crawling/page/UploadImage.java
@@ -3,6 +3,7 @@ package dev.ioexception.crawling.page;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+
 @Component
 // @RequiredArgsConstructor
 public class UploadImage {
@@ -25,26 +27,38 @@ public class UploadImage {
 
         // S3에 저장할 파일명 생성
         String fileName = dirName + "/" + filename + ".jpg";  // 확장자는 이미지 종류에 따라 변경
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        if (imageBytes != null) {
+            objectMetadata.setContentLength(imageBytes.length);
+        } else {
+            return "no image";
+        }
 
         // S3에 파일 업로드
-        amazonS3Client.putObject(new PutObjectRequest("classmoaimage", fileName, new ByteArrayInputStream(imageBytes), null)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
+        amazonS3Client.putObject(
+                new PutObjectRequest("classmoaimage", fileName, new ByteArrayInputStream(imageBytes), objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
 
         // 업로드된 이미지 URL 반환
         return amazonS3Client.getUrl("classmoaimage", fileName).toString();
     }
 
     private byte[] readImageBytes(String imageUrl) throws IOException {
-        URL url = new URL(imageUrl);
+        try {
+            URL url = new URL(imageUrl);
 
-        try (InputStream inputStream = url.openStream();
-             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
+            try (InputStream inputStream = url.openStream();
+                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                return outputStream.toByteArray();
             }
-            return outputStream.toByteArray();
+
+        } catch (Exception e) {
+            return null;
         }
     }
 }
