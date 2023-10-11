563a5cb850a39d2e1747c8d1b2ca6b65a0da2e43
임창준 <icj4153@hanyang.ac.kr>
2023-10-11 14:55:43 +0900

    [창준,가영,현철,규민] feat : Spring과 Opensearch 연결

--- crawling/src/main/java/dev/ioexception/crawling/controller/CrawlingController.java
+++ crawling/src/main/java/dev/ioexception/crawling/controller/CrawlingController.java
@@ -1,15 +1,32 @@
 package dev.ioexception.crawling.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+
+import dev.ioexception.crawling.aws.AWSRequestSigningApacheInterceptor;
 import dev.ioexception.crawling.service.CrawlingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CrawlingController {
 	private final CrawlingService crawlingService;
 
@@ -21,6 +38,52 @@ public class CrawlingController {
 		// crawlingService.getYbm();
 		crawlingService.getArtandStudy();
 		crawlingService.getInflearn();
+	}
+
+	@Value("${cloud.aws.region.static}")
+	private String region;//ap-northeast-2 서울리전
+
+	@GetMapping("/hi")
+	public String search1() throws IOException {
+		log.info("------------------------------------->search1");
+		RestHighLevelClient searchClient = searchClient();
+
+		// Create the document as a hash map
+		Map<String, Object> document = new HashMap<>();
+		document.put("title", "오픈서치 강의3");
+		document.put("instructor", "임창준3");
+		document.put("companyNmae", "카클스3");
+		document.put("ordinaryPrice", 100003);
+		document.put("salePrice", 70003);
+		document.put("salePercent", "33%");
+		document.put("imageLink", "naver.com3");
+
+		log.info("------------------------------------->Map");
+
+		// Form the indexing request, send it, and print the response
+		IndexRequest request = new IndexRequest("lectures", "_doc", "1").source(document);
+		IndexResponse response = searchClient.index(request, RequestOptions.DEFAULT);
+		System.out.println("response=========>" + response.toString());
+		System.out.println("response Result=========>" + response.getIndex());
+		return response.toString();
+
+	}
+
+	private static String serviceName = "es";
+	private static String host = "https://search-classmoa-uofe4bd5kkz5loqmz7wk4dpiqa.ap-northeast-2.es.amazonaws.com";
+
+	static final AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+
+
+	private RestHighLevelClient searchClient() {
+		log.info("------------------------------------->RestHighLevelClient");
+		AWS4Signer signer = new AWS4Signer();
+		signer.setServiceName(serviceName);
+		signer.setRegionName(region);
+		HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(serviceName, signer,
+			credentialsProvider);
+		return new RestHighLevelClient(RestClient.builder(HttpHost.create(host))
+			.setHttpClientConfigCallback(hacb -> hacb.addInterceptorLast(interceptor)));
 	}
 }
 
