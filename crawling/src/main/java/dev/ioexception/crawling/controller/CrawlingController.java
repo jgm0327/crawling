548ebe5c50a67437f901c00e32a1fe5b334730f4
임창준 <icj4153@hanyang.ac.kr>
2023-10-11 18:58:25 +0900

    [창준,가영] feat : 인덱스에 강의 도큐먼트 데이터 저장 및 조회

--- crawling/src/main/java/dev/ioexception/crawling/controller/CrawlingController.java
+++ crawling/src/main/java/dev/ioexception/crawling/controller/CrawlingController.java
@@ -1,7 +1,9 @@
 package dev.ioexception.crawling.controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.http.HttpHost;
@@ -21,6 +23,8 @@ import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 
 import dev.ioexception.crawling.aws.AWSRequestSigningApacheInterceptor;
+import dev.ioexception.crawling.entity.Lecture;
+import dev.ioexception.crawling.repository.LectureRepository;
 import dev.ioexception.crawling.service.CrawlingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +34,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CrawlingController {
 	private final CrawlingService crawlingService;
+	private final LectureRepository lectureRepository;
 
 	@GetMapping("/")
 	public void crawling() throws IOException, InterruptedException {
@@ -46,29 +51,46 @@ public class CrawlingController {
 
 	@GetMapping("/hi")
 	public String search1() throws IOException {
+		crawlingService.getMega();
+
 		log.info("------------------------------------->search1");
 		RestHighLevelClient searchClient = searchClient();
 
 		// Create the document as a hash map
-		Map<String, Object> document = new HashMap<>();
-		document.put("title", "오픈서치 강의2");
-		document.put("instructor", "임창준");
-		document.put("companyNmae", "카클스");
-		document.put("ordinaryPrice", 10000);
-		document.put("salePrice", 7000);
-		document.put("salePercent", "30%");
-		document.put("imageLink", "naver.com");
+		// Map<String, Object> document = new HashMap<>();
+		// document.put("title", "오픈서치 강의2");
+		// document.put("instructor", "임창준");
+		// document.put("companyName", "카클스");
+		// document.put("ordinaryPrice", 10000);
+		// document.put("salePrice", 8000);
+		// document.put("salePercent", "30%");
+		// document.put("imageLink", "naver.com");
 
 		log.info("------------------------------------->Map");
 
-		// Form the indexing request, send it, and print the response
-		IndexRequest request1 = new IndexRequest();
-		request1 = request1.opType(DocWriteRequest.OpType.INDEX).index("lecture").source(document);
-		IndexResponse response = searchClient.index(request1, RequestOptions.DEFAULT);
-		System.out.println(response.getResult().name());
+		List<Lecture> lectures = lectureRepository.findAllByDate(LocalDate.now());
+		for (Lecture lecture : lectures) {
+			// Form the indexing request, send it, and print the response
+			Map<String, Object> document = new HashMap<>();
+			document.put("title", lecture.getTitle());
+			document.put("instructor", lecture.getInstructor());
+			document.put("companyName", lecture.getCompanyName());
+			document.put("ordinaryPrice", lecture.getOrdinaryPrice());
+			document.put("salePrice", lecture.getSalePrice());
+			document.put("salePercent", lecture.getSalePercent());
+			document.put("imageLink", lecture.getImageLink());
 
-		System.out.println("response=========>" + response.toString());
-		System.out.println("response Result=========>" + response.getIndex());
+			IndexRequest request = new IndexRequest();
+			request = request.opType(DocWriteRequest.OpType.INDEX).index("lecture").id(lecture.getLectureId()).source(document);
+			searchClient.index(request, RequestOptions.DEFAULT);
+		}
+
+
+		// 리스폰스
+		// IndexResponse response = searchClient.index(request1, RequestOptions.DEFAULT);
+		// System.out.println(response.getResult().name());
+		// System.out.println("response=========>" + response.toString());
+		// System.out.println("response Result=========>" + response.getIndex());
 
 		return null;
 
--- crawling/src/main/java/dev/ioexception/crawling/repository/LectureRepository.java
+++ crawling/src/main/java/dev/ioexception/crawling/repository/LectureRepository.java
@@ -1,5 +1,7 @@
 package dev.ioexception.crawling.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import dev.ioexception.crawling.entity.Lecture;
@@ -9,4 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
 	Optional<Lecture> findLectureByLectureId(String lectureId);
+
+	List<Lecture> findAllByDate(LocalDate localDate);
 }
